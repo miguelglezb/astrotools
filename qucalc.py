d26daac42d5056a3951248121adf729f44d30ba1
miguelglezb <miguelgb@astro.unam.mx>
2021-07-02 09:39:19 +1000

    Add quick functions to calculate stellar dynamical time and binary orbial period (uses dataread module)

--- qucalc.py
+++ qucalc.py
@@ -3,6 +3,8 @@
 
 import numpy as np
 import scipy as sc
+import dataread as dr
+
 
 def mu_full_ion(x,y,z):
     print("X+Y+Z=",round(x+y+z,4))
@@ -15,4 +17,32 @@ def roche_lobe_sep(m1,m2,r1):
     print("Binary separation: ",round(A,2))
     print("Mass ratio: ",round(q,2))
     return [A,q]
-    
+
+
+def tdyn(M,R,units='phantom'):
+    seg = dr.constants.time
+    day = dr.constants.day
+    yr = dr.constants.yr
+    if units=='phantom' or units=='ph':
+        K = 1
+    elif units=='seg' or units=='s':
+        K = seg
+    elif units=='day' or units=='d':
+        K = day
+    elif units=='year' or units=='yr' or units=='y':
+        K = yr
+    return K*np.sqrt(R**3/M)
+
+def T_bin(M,a,units='phantom'):
+    seg = dr.constants.time
+    day = dr.constants.day
+    yr = dr.constants.yr
+    if units=='phantom' or units=='ph':
+        K = 1
+    elif units=='seg' or units=='s':
+        K = seg
+    elif units=='day' or units=='d':
+        K = day
+    elif units=='year' or units=='yr' or units=='y':
+        K = yr
+    return K*2*pi*np.sqrt(a**3/M)
